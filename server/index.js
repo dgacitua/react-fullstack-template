0ac0430bd2029e2aa9c2c699a0bdec35ecb6f282
Daniel Gacitua <daniel.gacitua@usach.cl>
2018-06-07 21:36:20 -0400

    Add client static files in production mode

--- server/index.js
+++ server/index.js
@@ -3,6 +3,6 @@ import os from 'os';
 
 const app = express();
 
-app.use(express.static('dist'));
+if (process.env.NODE_ENV === 'production') app.use('/', express.static('./dist/client'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 app.listen(8080, () => console.log('Listening on port 8080!'));
